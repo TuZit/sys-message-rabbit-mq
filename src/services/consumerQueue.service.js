e163ad318936759dba19826c5b9edddc8ec5630c
TuZitt <papadracula141@gmail.com>
2025-07-31 23:02:52 +0700

    Cách xử lý, throw message rabbitMQ lỗi logic

--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -18,20 +18,46 @@ class MessageServices {
     try {
       const { channel } = await connectToRabbitMQ();
 
-      const timeExpired = 15000;
-      setTimeout(() => {
-        channel.consume(
-          notifyQueue,
-          (msg) => {
-            console.log(
-              `Normal consumer receive new message: ${msg.content.toString()}`
-            );
-          },
-          {
-            noAck: true,
+      // Giả lập lỗi TTL
+      // const timeExpired = 15000;
+      // setTimeout(() => {
+      //   channel.consume(
+      //     notifyQueue,
+      //     (msg) => {
+      //       console.log(
+      //         `Normal consumer receive new message: ${msg.content.toString()}`
+      //       );
+      //     },
+      //     {
+      //       noAck: true,
+      //     }
+      //   );
+      // }, timeExpired);
+
+      // Giả lập lỗi logic
+      channel.consume(notifyQueue, (msg) => {
+        try {
+          const numberTest = Math.random();
+
+          if (numberTest < 0.8) {
+            console.log({ numberTest });
+            throw new Error("Send notification failed:: HOT FIX");
           }
-        );
-      }, timeExpired);
+
+          console.log(
+            "SEND notificationQueue sucessfully processed:",
+            msg.content.toString()
+          );
+          channel.ack(msg);
+        } catch (error) {
+          // console.error(error);
+          /**
+           * báo với RabbitMQ rằng một consumer không xử lý được một message.
+           * neck: có nhiệm vụ nếu có lỗi sẽ push cái message vào cái queue hứng lỗi
+           */
+          channel.nack(msg, false, false);
+        }
+      });
     } catch (error) {
       console.error(error);
     }
@@ -81,3 +107,19 @@ class MessageServices {
 }
 
 export default new MessageServices();
+
+/**
+ * Trong RabbitMQ, channel.nack(msg, requeue, multiple) là một phương thức
+ * để báo với RabbitMQ rằng một consumer không xử lý được một message. Nó là viết tắt của "negative acknowledgement".
+ * msg: Là message mà consumer muốn nack.
+ * requeue: Nếu là true, message sẽ được đưa trở lại queue và có thể được deliver cho một consumer khác (hoặc chính consumer này).
+ * Nếu là false, message sẽ bị loại bỏ hoặc chuyển đến một Dead Letter Exchange (DLX) nếu nó được cấu hình.
+ * multiple: Nếu là true, nó báo hiệu rằng consumer từ chối tất cả các message chưa được acknowledged cho đến message hiện tại.
+ * Điều này chỉ nên được sử dụng nếu bạn chắc chắn rằng tất cả các message trước đó cũng không thể xử lý được.
+ * Nếu là false, nó chỉ từ chối message hiện tại.
+ * Trong đoạn code trên, channel.nack(msg, false, false) có nghĩa là:
+ * Consumer báo với RabbitMQ rằng nó không thể xử lý message msg.
+ * Message msg sẽ không được đưa trở lại queue (requeue = false).
+ * Thay vào đó, nó sẽ bị loại bỏ hoặc chuyển đến DLX nếu có.
+ * Chỉ có message msg bị từ chối (multiple = false).
+ */
